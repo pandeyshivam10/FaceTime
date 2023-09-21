207fcd745c957c101ee463c7c422914abbd4fcb8
Shivam Pandey <shivampandey12345678910@gmail.com>
2023-09-21 17:20:12 +0530

    port removed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const { Server } = require("socket.io");
 
-const io = new Server(8000, {
-  cors: true,
+const io = new Server({
+  cors: {
+    origin: "https://face-time.vercel.app",
+    methods: ["GET", "POST"],
+  },
 });
 
 const emailToSocketIdMap = new Map();
